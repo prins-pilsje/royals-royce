5d10647129541f499b130489cd0ee00c4f81ac95
Quaese <quaese@gmx.net>
2014-01-25 08:33:13 +0100

    Layout mit horizontalem Scrolling erstellt

--- js/royals_royce.js
+++ js/royals_royce.js
@@ -1,16 +1,15 @@
 (function($){
-	$(function(){
-		var makeSlidable = function(index){
-			var btnDropDown = $('#drop_down_btn' + index),
-				divDropDown = $('#drop_down' + index);
+    $(function(){
+        var makeSlidable = function(index){
+            var btnDropDown = $('#drop_down_btn' + index),
+                divDropDown = $('#drop_down' + index);
 
-			btnDropDown.on('click', function(){
-				divDropDown.slideToggle();
-			});
-		}
+            btnDropDown.on('click', function(){
+                divDropDown.slideToggle();
+            });
+        }
 
-		makeSlidable("");
-		makeSlidable("2");
-
-	})
+        makeSlidable("");
+        makeSlidable("2");
+    })
 })(jQuery);
