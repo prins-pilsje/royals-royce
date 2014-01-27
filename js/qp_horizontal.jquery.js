fbd853add3b81caf152d67e6c22430a47ed37de6
unknown <mhoellein@mhoellein.intern.epages.de>
2014-01-27 14:58:11 +0100

    * CSS3 transition zugefügt * browserabhängiges Verhalten eingefügt

--- js/qp_horizontal.jquery.js
+++ js/qp_horizontal.jquery.js
@@ -12,7 +12,7 @@
                 anim: 1200
             }, options),
             preventDefaultEvents = true,
-            startX, startY;
+            startX, startY, isCss;
 
 
 
@@ -28,33 +28,30 @@
                 'left': 0
             });
 
+            // Testen, ob CSS-Eigenschaft existiert
+            isCss = checkCssProperty('transition');
+
             // Breite für Teil-Inhalte festlegen
             slides.each(function(index, element){
                 $(element).width(defaults.width);
             });
 
-            // DOMMouseScroll wegen Firefox ab Version 3
-            $win.on("mousewheel.qpHorPara DOMMouseScroll.qpHorPara", function(evt){
-                hMouseWheelHandler(evt);
-            });
-
-            if ('ontouchstart' in document.documentElement) {
-                //this.addEventListener('touchstart', onTouchStart, false);
-                $win.on('touchstart.qpHorPara', function(evt){
-                    $('.output').html(evt.originalEvent);
-                    onTouchStart(evt.originalEvent);
-                });
-            }
+            // Scroll- und Touchevents registrieren
+            registerEvents();
 
             return elem;
         };
-
-        function onTouchStart(e){
-            if (e.touches.length === 1) {
-                startX = e.touches[0].pageX;
-                startY = e.touches[0].pageY;
+/* *** [Events] *** */
+        function onTouchStart(evt){
+            if (evt.touches.length === 1) {
+                startX = evt.touches[0].pageX;
+                startY = evt.touches[0].pageY;
                 isMoving = true;
-                this.addEventListener('touchmove', onTouchMove, false);
+
+                //this.addEventListener('touchstart', onTouchStart, false);
+                $win.on('touchmove.qpHorPara', function(evt){
+                    onTouchMove(evt.originalEvent);
+                });
             }
         }
 
@@ -70,7 +67,7 @@
                     dy = startY - y,
                     delta = 0;
 
-                $win.off("touchstart.qpHorPara");
+                $win.off("touchstart.qpHorPara touchmove.qpHorPara");
 
                 if(dx > 0) {
                     delta = 1;
@@ -98,32 +95,25 @@
             evt.preventDefault();
 
             var origEvt = evt.originalEvent,                                                    // Orginalevent
-                delta = -Math.max(-1, Math.min(1, (origEvt.wheelDelta || -origEvt.detail))),    // -1 oder 1
-                addLeft = delta * defaults.width,                                               // Left-Differenz
-                left = parseInt(list.css('left')),                                              // bisheriger left-Wert
-                newLeft = left - addLeft;                                                       // neuer left-Wert
-
-            // // falls der neue left-Wert im gültigen Bereich liegt
-            // if((newLeft <= 0) && (newLeft >= -defaults.listWidth+defaults.width)){
-            //     list.animate({
-            //         left: newLeft + "px",
-            //         easing: 'easeInOutQuart'
-            //     }, defaults.anim, function() {
-            //         // DOMMouseScroll wegen Firefox ab Version 3
-            //         $win.on("mousewheel.qpHorPara DOMMouseScroll.qpHorPara", function(evt){
-            //             hMouseWheelHandler(evt);
-            //         });
-            //     });
-            // }else{
-            //     // DOMMouseScroll wegen Firefox ab Version 3
-            //     $win.on("mousewheel.qpHorPara DOMMouseScroll.qpHorPara", function(evt){
-            //         hMouseWheelHandler(evt);
-            //     });
-            // }
+                delta = -Math.max(-1, Math.min(1, (origEvt.wheelDelta || -origEvt.detail)));    // -1 oder 1
 
             animSlide(delta);
         }
 
+        function registerEvents(){
+            // DOMMouseScroll wegen Firefox ab Version 3
+            $win.on("mousewheel.qpHorPara DOMMouseScroll.qpHorPara", function(evt){
+                hMouseWheelHandler(evt);
+            });
+            if ('ontouchstart' in document.documentElement) {
+                //this.addEventListener('touchstart', onTouchStart, false);
+                $win.on('touchstart.qpHorPara', function(evt){
+                    onTouchStart(evt.originalEvent);
+                });
+            }
+        }
+
+/* *** [Animation] *** */
         function animSlide(delta) {
 
             var addLeft = delta * defaults.width,                                               // Left-Differenz
@@ -132,36 +122,57 @@
 
             // falls der neue left-Wert im gültigen Bereich liegt
             if((newLeft <= 0) && (newLeft >= -defaults.listWidth+defaults.width)){
-                list.animate({
-                    left: newLeft + "px",
-                    easing: 'easeInOutQuart'
-                }, defaults.anim, function() {
-                    // DOMMouseScroll wegen Firefox ab Version 3
-                    $win.on("mousewheel.qpHorPara DOMMouseScroll.qpHorPara", function(evt){
-                        hMouseWheelHandler(evt);
-                    });
-                        if ('ontouchstart' in document.documentElement) {
-                        //this.addEventListener('touchstart', onTouchStart, false);
-                        $win.on('touchstart.qpHorPara', function(evt){
-                            $('.output').html(evt.originalEvent);
-                            onTouchStart(evt.originalEvent);
-                        });
-                    }
-                });
-            }else{
-                // DOMMouseScroll wegen Firefox ab Version 3
-                $win.on("mousewheel.qpHorPara DOMMouseScroll.qpHorPara", function(evt){
-                    hMouseWheelHandler(evt);
-                });
 
-                if ('ontouchstart' in document.documentElement) {
-                    //this.addEventListener('touchstart', onTouchStart, false);
-                    $win.on('touchstart.qpHorPara', function(evt){
-                        $('.output').html(evt.originalEvent);
-                        onTouchStart(evt.originalEvent);
+                if(isCss){
+                    list.css({
+                        'left': newLeft + "px",
+                        'transition': 'left ' + (defaults.anim/1000)+ 's ease-in-out'
+                    });
+
+                    window.setTimeout(function(){
+                        registerEvents();
+                    }, defaults.anim);
+                }else{
+                    list.animate({
+                        left: newLeft + "px",
+                        easing: 'easeInOutQuart'
+                    }, defaults.anim, function() {
+                        registerEvents();
                     });
                 }
+
+            }else{
+                registerEvents();
             }
+        }
+
+/* *** [HILFSFUNKTIONEN] *** */
+        // Existenz einer CSS-Eigenschaft testen
+        function checkCssProperty(prop) {
+            var elem = document.createElement('div'),
+                prefixes = ['Moz', 'Webkit', 'O', 'ms'],
+                _prop, i, prefixedProp;
+
+            // testen auf Eigenschaft ohne Präfix
+            if (prop in elem.style) {
+                return true;
+            }
+
+            _prop = prop.charAt(0).toUpperCase() + prop.substr(1);
+
+            if (prop in elem.style) {
+                return true;
+            }
+
+            for (i=0; i<prefixes.length; ++i) {
+                var prefixedProp = prefixes[i] + _prop;
+
+                if (prefixedProp in elem.style) {
+                    return true;
+                }
+            }
+
+            return false;
         }
 
         // Public:
